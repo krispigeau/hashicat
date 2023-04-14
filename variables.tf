01b2775583032d6ca56c11489c3e17cec2473efe
Kris Pigeau <kris.pigeau@gmail.com>
2023-04-14 14:21:46 -0400

    change hello world

--- variables.tf
+++ variables.tf
@@ -24,6 +24,11 @@ variable "placeholder" {
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
 
+variable "hello" {
+  default     = "Meow"
+  description = "change the word in hello world statment"
+}
+
 variable "environment" {
   description = "Environment parameter to provide uniqueness per environment"
   default     = "development"
